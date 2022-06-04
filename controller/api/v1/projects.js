2c60a983a4431f006280de7170f3797b4362d2a2
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2022-06-04 13:05:04 +0200

    refactor(projects): add try and cath to controller

--- controller/api/v1/projects.js
+++ controller/api/v1/projects.js
@@ -2,30 +2,47 @@ const Project = require('../../../models/api/v1/Project');
 
 
 const getAll = async (req, res) => {
-    let projects = await Project.find();
-    res.json({
-        "status": "success",
-        "data": {
-            "projects": projects
-        }
-    });
+    try {
+        let projects = await Project.find();
+        res.json({
+            "status": "success",
+            "data": {
+                "projects": projects
+            }
+        });
+    }
+    catch {
+        res.json({
+            "status": "error",
+            "message": "Couldn't get all your projects"
+        });
+    }
 }
 
 const create = async (req, res) => {
-    let project = new Project();
-    project.title = req.body.title;
-    project.description = req.body.description;
-    project.picture = req.body.picture;
-    project.tags = req.body.tags;
-    project.link = req.body.link;
-    let newProject = await project.save()
+    try {
 
-    res.json({
-        "status": "success",
-        "data": {
-            "project": newProject
-        }
-    });
+        let project = new Project();
+        project.title = req.body.title;
+        project.description = req.body.description;
+        project.picture = req.body.picture;
+        project.tags = req.body.tags;
+        project.link = req.body.link;
+        let newProject = await project.save()
+        
+        res.json({
+            "status": "success",
+            "data": {
+                "project": newProject
+            }
+        });
+    }
+    catch {
+        res.json({
+            "status": "error",
+            "message": "Couldn't create your project"
+        })
+    }
 }
 
 module.exports = {
